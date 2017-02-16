b9a92b6910ba2825e8d287ff4e25a5e28eac0676
edggy <edggy@Ethan-Alienware>
2017-02-16 17:33:37 -0500

    Huffman encoding works with bytes

--- huffman.py
+++ huffman.py
@@ -1,6 +1,9 @@
 import math
+import Queue
+import copy
 
 from BitStream.bitStream import BitStream
+from FrequencyCounter.freqCounter import FreqCounter
 
 def numToBytearray(num):
     result = bytearray()
@@ -25,6 +28,9 @@ def encodeNum(num):
         # 3      1010
         # 4      1011
         # 5      1100000
+        # 6      1100001
+        # 7      1100010
+        # 8      1100011
         
         try:
             tmp = 0
@@ -92,7 +98,8 @@ class BinaryTree:
             length = 0
             for c in data:
                 length += 8
-            self.dataLength = int(math.floor(math.log(length,2)) + 1)
+            #self.dataLength = int(math.floor(math.log(length,2)) + 1)
+            self.dataLength = length
         except (TypeError, ValueError):
             self.dataLength = 1
             
@@ -119,28 +126,51 @@ class BinaryTree:
     def __lt__(self, other):
         return self.data < other
     
-    def toStream(self, stream, first = True):
-        if first:
-            stream += encodeNum(self.dataLength).reverse()
+    def toStream(self, stream, dataLength = None):
+        if dataLength is None:
+            #print 'toStream: dataLength %s' % self.dataLength
+            dataLength = self.dataLength
+            stream += encodeNum(dataLength)
         
         if self.left is None and self.right is None:
+            #print 'toStream: Adding Leaf %s %s' % (self.data, BitStream(self.data, dataLength))
             stream.push(1)
-            numStream = encodeNum(self.data)
-            stream.push(numStream, len(numStream))
+            stream.push(self.data, dataLength)
+            #print 'toStream: %s' % stream
+            #numStream = encodeNum(self.data)
+            #stream.push(numStream, len(numStream))
             
         else:
+            #print 'toStream: Adding subtrees'
             stream.push(0)
-            self.left.toStream(stream, False)
+            #print 'toStream: %s' % stream
+            self.left.toStream(stream, dataLength)
             
-            self.right.toStream(stream, False)
+            self.right.toStream(stream, dataLength)
+            #print 'toStream: %s' % stream
             
 
-import Queue
-
-def bitStreamToTree(stream):
-    num = decodeNum(stream)
+def bitStreamToTree(stream, dataLength = None):
+    '''
+    Takes a BitStream and parses a BinaryTree from the data
     
-    print stream
+    @param stream - A BitStream of data starting with a BinaryTree
+    @return - The parsed BinaryTree
+    '''
+    
+    if dataLength is None:
+        dataLength = decodeNum(stream)
+        
+    if stream.pop() == 1:
+        # We have data
+        data = BitStream()
+        for i in xrange(dataLength):
+            data.push(stream.pop())
+        return BinaryTree(data)
+    else:
+        leftTree = bitStreamToTree(stream, dataLength)
+        rightTree = bitStreamToTree(stream, dataLength)
+        return BinaryTree(None, leftTree, rightTree)
     
 
 
@@ -159,6 +189,9 @@ def huffmanTree(freqMap):
     return queue.get()[1]
 
 def huffmanTreeToDict(hTree):
+    '''
+    Converts a Huffman tree to a Dictionary
+    '''
     stack = Queue.LifoQueue()
     stack.put((hTree, BitStream()))
     d = {}
@@ -169,23 +202,56 @@ def huffmanTreeToDict(hTree):
         else:
             stack.put((node.left, path + 1))
             stack.put((node.right, path + 0))
-            
-    return d
-
-
-from FrequencyCounter.freqCounter import FreqCounter
+    
+    dp = {}
+    for key in d:
+        dp[d[key]] = key
+    return d, dp
 
 def encode(string):
+    '''
+    Takes a string and compresses it using Huffman Encoding
+    
+    @param string - Input to compress
+    @return - A BitStream of the tree followed by the compressed data
+    '''
     fc = FreqCounter(string, 1)
     ht = huffmanTree(fc.data)
-    print ht
-    d = huffmanTreeToDict(ht)
-    print d
+    print 'encode: %s' % ht
+    d, dp = huffmanTreeToDict(ht)
+    print 'encode: %s' % d
     stream = BitStream()
     ht.toStream(stream)
+    print len(stream)
     for c in string:
         stream += d[(c,)]
+    print len(stream)
     return stream
+
+def decode(stream):
+    '''
+    Takes a BitStream and decompresses it into the original data
+    '''
+    
+    ht = bitStreamToTree(stream)
+    d, dp = huffmanTreeToDict(ht)
+    print dp
+    print len(stream)
+    #output = BitStream()
+    output = ''
+    tok = BitStream()
+    for b in stream:
+        tok.push(b)
+        if tok in dp:
+            #output.push(dp[tok])
+            data = copy.copy(dp[tok])
+            while len(data) > 0:
+                dataPart = BitStream()
+                for i in xrange(8):
+                    dataPart += data.pop()
+                output += chr(int(dataPart))
+            tok = BitStream()
+    return output
     
 
 if __name__ == '__main__':    
@@ -196,7 +262,7 @@ if __name__ == '__main__':
     
     for i in xrange(658):
         stream = encodeNum(i)
-        enc = int(stream)
+        enc = int(stream.stream)
         stream2 = encodeNum(i)
         print '%s = %s = %s' % (i, stream.reverse(), decodeNum(stream2))    
         
@@ -212,7 +278,16 @@ if __name__ == '__main__':
     print huffmanTreeToDict(ht)
     
     print
-    ht2 = encode('asdfasdfaaaafaafdsssadaafgddsaaaasdsaaassddfsdaaadaaaaad')
-    print ht2
+    import sys
+    #inputData = 'asdfasdfaaaafaafdsssadaafgddsaaaasdsaaassddfsdaaadaaaaad'
+    filename = sys.argv[1]
+    with open(filename, mode='rb') as f:
+        inputData = f.read()
+        
+    inputSize = len(inputData)*8
+    stream3 = encode(inputData)
+    outputSize = len(stream3)
+    #print stream3
+    print 'Compressed from %d to %d bits. A factor of %f%%' % (inputSize, outputSize, 100.0*outputSize/inputSize)
     
-    print bitStreamToTree(ht2)
+    print decode(stream3)
